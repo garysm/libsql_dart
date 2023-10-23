0b609792bb9072a8b2c06900e88c45711bca669b
garysm <gary@sabiasoftware.com>
2023-10-23 14:56:06 -0400

    initial commit

---
+++ example/libsql_dart_example.dart
@@ -0,0 +1,6 @@
+// import 'package:libsql_dart/libsql_dart.dart';
+
+// void main() {
+//   var awesome = Awesome();
+//   print('awesome: ${awesome.isAwesome}');
+// }
---
+++ test/libsql_dart_test.dart
@@ -0,0 +1,16 @@
+// import 'package:libsql_dart/libsql_dart.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   group('A group of tests', () {
+//     final awesome = Awesome();
+
+//     setUp(() {
+//       // Additional setup goes here.
+//     });
+
+//     test('First Test', () {
+//       expect(awesome.isAwesome, isTrue);
+//     });
+//   });
+// }
